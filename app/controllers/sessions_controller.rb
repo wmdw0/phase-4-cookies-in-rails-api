f3df3805c036b76b2d2609779df44a503c7987a5
William Walsh <wmdwalsh0@gmail.com>
2021-12-20 13:28:04 -0500

    completed assignment

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,15 @@
 class SessionsController < ApplicationController
   
+  # def index
+  #   session[:session_hello] ||= "World"
+  #   cookies[:cookies_hello] ||= "World"
+  #   render json: { session: session, cookies: cookies.to_hash }
+  # end
+
   def index
+    byebug
     session[:session_hello] ||= "World"
     cookies[:cookies_hello] ||= "World"
     render json: { session: session, cookies: cookies.to_hash }
   end
-
 end
